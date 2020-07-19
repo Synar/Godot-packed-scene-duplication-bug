267488b0f1e8bd5483c7f64a70c275af9a1b2395
Synar <hakim.khoudli@polytechnique.org>
2020-07-19 17:37:25 +0200

    duplication bug made more obvious

--- Ennemy.gd
+++ Ennemy.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 
 func _ready():
-    print("sprite from ennemy : ",$Sprite)
-    print("label from ennemy : ",$Sprite/Label)
+    print("ennemy :",self)
+    print("ennemy children :")
+    for child in get_children():
+        print("    child :", child)
+    #print("sprite from ennemy : ",$Sprite)
+    #print("label from ennemy : ",$Sprite/Label)
 
 
